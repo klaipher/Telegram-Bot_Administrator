69c79bc9c3f0a051c45bd6ffb1aeef5ec494d6c8
Dima Miskevich <33063280+DimaMiskevich@users.noreply.github.com>
2018-09-30 16:23:35 +0300

    New config for webhook

--- bot/config.py
+++ bot/config.py
@@ -9,3 +9,15 @@ DB = {
 }
 MY_ID =   # Ваш Telegram id
 MY_CHANNEL = ''  # Ваш Telegram канал
+
+WEBHOOK_HOST = ''  # Domain name or IP addres which your bot is located.
+WEBHOOK_PORT = 443  # Telegram Bot API allows only for usage next ports: 443, 80, 88 or 8443
+WEBHOOK_URL_PATH = '/webhook'  # Part of URL
+
+# This options needed if you use self-signed SSL certificate
+# Instructions: https://core.telegram.org/bots/self-signed
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+WEBAPP_HOST = ''
+WEBAPP_PORT = 443
